65ce9407dc590168d0b7d3c2f757fb005e32d02c
Sameer Khan <sk5749857@gmail.com>
2026-04-15 05:49:34 -0700

    Add files via upload

---
+++ filCreateWrite.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main() {
+  File file = File("intro.txt");
+  file.writeAsStringSync("Ayesha Bawani university");
+  print("File Created And Data Entered! Updated");
+}
+
+
+//C:\Users\HP\Documents\index.dart\File_Handel.dart
